24306212edd7af8b5d7a5a15c02ffba78246587f
huong1999 <huongyaya@gmail.com>
2022-08-02 13:01:04 +0700

    edit homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,37 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
- 
-import { FormGroup, FormBuilder, Validators } from '@angular/forms'
- 
-
-
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 @Component({
-  selector: 'app-homepage',
-  templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss'],
+  selector: "app-homepage",
+  templateUrl: "./homepage.component.html",
+  styleUrls: ["./homepage.component.scss"],
 })
 export class HomepageComponent implements OnInit {
-  constructor(private router: Router, private route: ActivatedRoute, private fb: FormBuilder) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private fb: FormBuilder
+  ) {}
   hide: boolean = false;
 
   ngOnInit(): void {}
-  
-  loginForm: FormGroup = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]]
-  })
 
-onLogin() {
+  loginForm: FormGroup = this.fb.group({
+    email: ["", [Validators.required, Validators.email]],
+    password: ["", [Validators.required, Validators.minLength(6)]],
+  });
+
+  onLogin() {
     if (!this.loginForm.valid) {
       return;
     }
     console.log(this.loginForm.value);
   }
-  onOpenPage() {
-    //this.router.navigate(['/exercise1'])
-  }
-
   gotoUser() {
-    this.router.navigateByUrl('user');
+    this.router.navigateByUrl("user");
   }
 }
